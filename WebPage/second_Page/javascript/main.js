79fda00817ec42f3bdedc69f6f3836102d205c4e
Archana1660 <archanamech16@gmail.com>
2020-03-27 23:38:38 +0530

    'navigation'

--- WebPage/second_Page/javascript/main.js
+++ WebPage/second_Page/javascript/main.js
@@ -1,8 +1,17 @@
-function menu(){
-    let menuList = document.querySelector('.menu-list');
-    if(menuList.style.display === "block") {
-         menuList.style.display = "none";
-    } else {
-     menuList.style.display = "block";
-    }
-}
+
+const menuList = document.querySelector('.menu-list');
+const burgerMenu = document.querySelector('.burger-icon');
+const links = document.querySelectorAll('.menu-list ul li');
+const firstLine = document.querySelector('.firstLine');
+const secondLine = document.querySelector('.secondLine');
+const thirdLine = document.querySelector('.thirdLine');
+
+burgerMenu.addEventListener('click', () => {
+    menuList.classList.toggle('open');
+})
+
+burgerMenu.addEventListener('click', () => {
+    firstLine.classList.toggle('firstLineChange');
+    secondLine.classList.toggle('secondLineChange');
+    thirdLine.classList.toggle('thirdLineChange');
+})
