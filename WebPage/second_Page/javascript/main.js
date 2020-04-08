38d386d26c9d5a0703370c44d81702b110a03c0d
Archana1660 <archanamech16@gmail.com>
2020-04-08 21:51:40 +0530

    updated active nav links and scroll on nav

--- WebPage/second_Page/javascript/main.js
+++ WebPage/second_Page/javascript/main.js
@@ -1,27 +1,73 @@
-
 const menuList = document.querySelector('.menu-list');
-const bodyScroll = document.querySelector('body');
 const burgerMenu = document.querySelector('.burger-icon');
-const links = document.querySelectorAll('.menu-list ul li');
+const body = document.querySelector('body');
+const navLinks = document.querySelectorAll("nav ul li a");
 const firstLine = document.querySelector('.firstLine');
 const secondLine = document.querySelector('.secondLine');
 const thirdLine = document.querySelector('.thirdLine');
+const submitBtn = document.querySelector('.submit');
+const loader = document.querySelector('.loader');
+const toShow = document.querySelector('.opaque')
+
+// window.addEventListener("scroll", (event) => {
+//     let fromTop = window.scrollY;
+//     console.log(fromTop)
+//     navLinks.forEach((link) => {
+//       let section = document.querySelector(link.hash);
+//       if (
+//         section.offsetTop <= fromTop &&
+//         section.offsetTop + section.offsetHeight > fromTop
+//       ) {
+//         link.classList.add("current");
+//       } else {
+//         link.classList.remove("current");
+//       }
+//     });
+//   });
 
 burgerMenu.addEventListener('click', () => {
     menuList.classList.toggle('open');
-    bodyScroll.classList.toggle('forScroll');
-})
-
-burgerMenu.addEventListener('click', () => {
+    body.classList.toggle('forScroll');
     firstLine.classList.toggle('firstLineChange');
     secondLine.classList.toggle('secondLineChange');
     thirdLine.classList.toggle('thirdLineChange');
+});
+
+submitBtn.addEventListener('click', () => {
+    loader.style.display = "inline-block";
 })
 
-document.addEventListener('scroll', window, function (event) {
-    // get current scroll position  
-    var currentScrollPos =    window.scrollTop;
-    if(currentScrollPos >= 100) {
-        // show your div
+
+var myScrollFunc = function() {
+    var y = window.scrollY;
+    console.log(`y is ${y}`);
+
+    if (y < 100) {
+        toShow.style.backgroundColor = "transparent";
+        menuList.style.opacity = 1;
+    } else if (y <= 649 && y >= 100) {
+        toShow.style.backgroundColor = "transparent";
+        menuList.style.opacity = 0;
+    } else {
+        toShow.style.backgroundColor = "#000";
+        menuList.style.opacity = 1;
     }
-}, false);
+
+
+    navLinks.forEach((link) => {
+        let section = document.querySelector(link.hash);
+
+        if (
+          section.offsetTop <= y &&
+          section.offsetTop + section.offsetHeight > y
+        ) {
+          link.classList.add("current");
+        } else {
+          link.classList.remove("current");
+        }
+      });
+};
+
+        
+window.addEventListener("scroll", myScrollFunc)     
+
